ae764d0ddd0e1feb56a24105d37f302d5dc1cf90
deleonjuan <juan.deleon.dev@gmail.com>
2024-08-05 16:10:58 -0600

    fix: added PORT to astro config

--- api/astro.config.mjs
+++ api/astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+  server: { port: parseInt(process.env.PORT) || 4321, host: true },
 });
